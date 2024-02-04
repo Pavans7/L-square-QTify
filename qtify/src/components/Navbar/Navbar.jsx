dbc1ccfa87b937fcc2241af820095617ebc31329
Pawan Shimpi <pavanshimpi786@gmail.com>
2024-02-04 17:22:39 +0530

    final commit

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -5,11 +5,19 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
 import Button from "../Button/Button";
-import styles from "./Navbar.module.css";
+import  "./Navbar.module.css";
 
 function Navbar({ searchData }) {
     return (
-        <nav className = {`${styles.navbar} nav`}>
+        <nav className = "nav" style={{
+            backgroundColor: 'var(--color-primary)',
+            height: '74px',
+            width: '100vw',
+            padding: '0px 32px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+        }}>
             <a href="/">
                 <Logo/>
             </a>
